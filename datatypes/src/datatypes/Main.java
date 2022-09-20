9f6804b7c79da96dbabfdbd76311ff6eeab97da6
erdmgnydnn <erdembakigunaydn28@gmail.com>
2022-09-20 20:28:12 +0300

    First comit

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,16 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayi= 12;
+		sayi = 13;
+		
+		char = "A";
+		
+		boolean dogruMu = false;
+		
+
+	}
+
+}
